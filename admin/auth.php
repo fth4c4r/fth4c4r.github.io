ce2e184db278ef1ca2a7608b1a579ed9aaf9f2f3
Fatih Acar <fatihacar79@gmail.com>
2026-07-02 16:45:21 +0300

    Delete admin directory

--- admin/auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__ . '/../includes/init.php';
-if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
-    header('Location: login.php');
-    exit;
-}
