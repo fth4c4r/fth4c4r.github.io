4ac4cb747df8591241a520273a9e9a0030b4e131
Fatih Acar <fatihacar79@gmail.com>
2026-07-02 17:15:23 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,36 @@
+fetch("data/projects.json")
+.then(response => response.json())
+.then(projects => {
+
+    const container =
+        document.getElementById("projectContainer");
+
+    projects.forEach(project => {
+
+        container.innerHTML += `
+        <div class="project-card">
+
+            <img src="${project.image}"
+                 alt="${project.title}">
+
+            <div class="project-content">
+
+                <h3>${project.title}</h3>
+
+                <p>${project.description}</p>
+
+                <a href="${project.url}"
+                   class="project-btn"
+                   target="_blank">
+                   İncele
+                </a>
+
+            </div>
+
+        </div>
+        `;
+    });
+})
+.catch(error => {
+    console.error(error);
+});
